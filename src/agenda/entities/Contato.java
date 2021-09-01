acec3be49f1583d11d769e3439c47ed4811d4d8f
Maira Fidalgo <mairaberlatof@gmail.com>
2021-09-01 16:24:58 -0300

    Criação da entidade Contato + modificação de launch.json

---
+++ src/agenda/entities/Contato.java
@@ -0,0 +1,31 @@
+package agenda.entities;
+
+public class Contato {
+  private String nome;
+  private int idade;
+  private String tel;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getIdade() {
+    return idade;
+  }
+
+  public void setIdade(int idade) {
+    this.idade = idade;
+  }
+
+  public String getTel() {
+    return tel;
+  }
+
+  public void setTel(String tel){
+    this.tel = tel;
+  }
+}
