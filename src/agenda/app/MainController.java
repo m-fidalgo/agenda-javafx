0da75168b199d1fb8acbc8e947a478facc146997
Maira Fidalgo <mairaberlatof@gmail.com>
2021-09-01 23:30:21 -0300

    Add das funcionalidades e controles

--- src/agenda/app/MainController.java
+++ src/agenda/app/MainController.java
@@ -2,6 +2,7 @@ package agenda.app;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import agenda.entities.Contact;
@@ -11,10 +12,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class MainController implements Initializable{
   @FXML
@@ -44,9 +48,24 @@ public class MainController implements Initializable{
   @FXML
   private Button btnSalvar;
 
+  private Boolean isInsert;
+
+  private Contact selectedContact;
+
   @Override
   public void initialize(URL location, ResourceBundle resources){
     this.tabelaContatos.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    habilitarEdicao(false);
+
+    this.tabelaContatos.getSelectionModel().selectedItemProperty().addListener((observador, contatoAntigo, contatoNovo) -> {
+      if(contatoNovo != null){
+        txtNome.setText(contatoNovo.getNome());
+        txtIdade.setText(String.valueOf(contatoNovo.getIdade()));
+        txtTel.setText(contatoNovo.getTel());
+        selectedContact = contatoNovo;
+      }
+    });
+
     carregarTabelaContatos();
   }
 
@@ -65,5 +84,71 @@ public class MainController implements Initializable{
     //observable pq assim ele repara qd a lista muda e já atualiza
     ObservableList<Contact> contatosObservableList = FXCollections.observableArrayList(contatos);
     this.tabelaContatos.getItems().setAll(contatosObservableList);
+  }
+
+  private void habilitarEdicao(Boolean isEdicaoHabilitada){
+    this.txtNome.setDisable(!isEdicaoHabilitada);
+    this.txtIdade.setDisable(!isEdicaoHabilitada);
+    this.txtTel.setDisable(!isEdicaoHabilitada);
+    this.btnCancelar.setDisable(!isEdicaoHabilitada);
+    this.btnSalvar.setDisable(!isEdicaoHabilitada);
+    this.btnInsert.setDisable(isEdicaoHabilitada);
+    this.btnUpdate.setDisable(isEdicaoHabilitada);
+    this.btnDelete.setDisable(isEdicaoHabilitada);
+    this.tabelaContatos.setDisable(isEdicaoHabilitada);
+  }
+
+  private void cleanFields() {
+    this.txtNome.setText("");
+    this.txtIdade.setText("");
+    this.txtTel.setText("");
+  }
+
+  public void btnInsert_Action() {
+    this.isInsert = true;
+    this.cleanFields();
+    habilitarEdicao(true);
+  }
+
+  public void btnUpdate_Action() {
+    this.isInsert = false;
+    habilitarEdicao(true);
+    this.txtNome.setDisable(true);
+  }
+
+  public void btnDelete_Action() {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Confirmação");
+    alert.setHeaderText("Tem certeza de que deseja excluir o contato?");
+    Optional<ButtonType> resp = alert.showAndWait();
+
+    if(resp.isPresent() && resp.get() == ButtonType.OK){
+      AgendaRepository<Contact> repContato = new ContactRepository();
+      repContato.delete(selectedContact);
+      carregarTabelaContatos();
+      this.tabelaContatos.getSelectionModel().selectFirst();
+    }
+  }
+
+  public void btnCancelar_Action() {
+    habilitarEdicao(false);
+    this.cleanFields();
+    this.tabelaContatos.getSelectionModel().selectFirst();
+  }
+
+  public void btnSalvar_Action() {
+    AgendaRepository<Contact> repContato = new ContactRepository();
+    Contact contato = new Contact();
+    contato.setNome(txtNome.getText());
+    contato.setIdade(Integer.parseInt(txtIdade.getText()));
+    contato.setTel(txtTel.getText());
+
+    if(this.isInsert)
+      repContato.insert(contato);
+    else repContato.update(contato);
+
+    habilitarEdicao(false);
+    carregarTabelaContatos();
+    this.tabelaContatos.getSelectionModel().selectFirst();
   }
 }
